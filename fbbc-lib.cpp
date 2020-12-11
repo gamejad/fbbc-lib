671a325628ab956153940d9edc0caa78ea94bca1
gamejad <vladislav2018@yandex.ru>
2020-12-11 17:36:04 +0300

    added classes and some functions

--- fbbc-lib.cpp
+++ fbbc-lib.cpp
@@ -1,1 +1,29 @@
 //fbbc-lib...
+#include "fbbc-lib.h"
+
+
+double RZtoEta(double r, double z){
+	double theta = atan(r/z);
+	return -log(tan(theta/2));
+}
+//--------------------------------------
+
+void MCSector::SetParticleTimes(const vector<double> times){
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> distribution(0.0,1.0);
+	for(const auto t : times){
+		double number = distribution(generator);
+		if(number <= efficiency){
+			part_times.push_back(t);
+		} else continue;
+	}
+	part_times.sort();
+}
+
+vector<double> MCSector::GetParticleTimes() const{
+	vector<double> result;
+
+}
+
+
+
--- fbbc-lib.h
+++ fbbc-lib.h
@@ -1,1 +1,72 @@
 //FBBC-lib...
+#pragma once
+
+#include <vector>
+#include <pair>
+#include <random>
+#include <cmath>
+
+#define PI = 3.14159265359;
+
+double RZtoEta();
+
+class MCSector {
+public:
+	MCSector(pair<double> phi_pair, pair<double> r_pair, double eff, double t_prec):
+		phi_limit(phi_pair),
+		r_limit(r_pair),
+		efficiency(eff),
+		time_prec(t_prec) {}
+
+	void SetParticleTimes(const vector<double> times);
+
+	vector<double> GetParticleTimes() const;
+private:
+	pair<double> phi_limit; //{phi1, phi2}
+	pair<double> r_limit;   // {r1, r2}
+	double efficiency = 1; // 100%
+	double time_prec = 0; //ps
+	vector<double> part_times;
+}
+
+
+class MCPlate {
+public:
+	MCPlate(double rin, double rout, double rad_num, double phi_num, double eff, double t_prec):
+		Rin(rin),
+		Rout(rout),
+		Rad_sec_num(rad_num),
+		Phi_sec_num(phi_num) {
+		double delta_r = (rout-rin)/rad_num;
+		double delta_phi = 2*PI/phi_num;
+		mc_sectors.resize(rad_num);
+		for(int i = 0; i < rad_num; i++){
+			pair<double> rads = {rin + i*delta_r, rin+(i+1)*delta_r};
+			for(int j = 0; j < phi_num; j++){
+				pair<double> phis = {j*delta_phi, (j+1)*delta_phi};
+				MCSector plate(rads, phis, eff, t_prec);
+				mc_sectors[i].push_back(plate);
+			}
+		}
+	}
+
+private:
+	vector<vector<MCSector>> mc_sectors;
+	double Rin, Rout; // inner and outter radiuses, in mm
+	size_t Rad_sec_num = 1;
+	size_t Phi_sec_num = 1;
+}
+
+class DetectorFacility {
+public:
+
+
+private:
+	vector<pair<MCPlate, double> > mcplates;
+	int plate_num = 0;
+	double Z_coordinate = 0;
+}
+
+
+
+
